c2a14e1ea0d7ca7cd1a5fc8f11fd71a84b4d7cba
jshfcnl <57579748+sherlocknt@users.noreply.github.com>
2022-03-30 11:01:20 +0800

    Add files via upload
    login php v1

---
+++ login.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+	<form action="index.html">
+		<input type="submit" value="continue">
+	</form>
+</body>
+</html>
