db2f33020ee0fddc875c2574dd04ab792d28bbaa
jshfcnl <57579748+sherlocknt@users.noreply.github.com>
2022-11-27 10:19:11 +0800

    Update app.js
    v11.27.22

--- map/app.js
+++ map/app.js
@@ -2,7 +2,7 @@
 var myLatLng = { lat: 15.486437, lng: 120.973429 };
 var mapOptions = {
     center: myLatLng,
-    zoom: 10,
+    zoom: 14,
     mapTypeId: google.maps.MapTypeId.ROADMAP
 
 };
@@ -26,7 +26,7 @@ function calcRoute() {
     var request = {
         origin: document.getElementById("from").value,
         destination: document.getElementById("to").value,
-        travelMode: google.maps.TravelMode.DRIVING, //WALKING, BYCYCLING, TRANSIT
+        travelMode: google.maps.TravelMode.WALKING, //WALKING, BICYCLING, TRANSIT
         unitSystem: google.maps.UnitSystem.IMPERIAL
     }
 
@@ -36,7 +36,7 @@ function calcRoute() {
 
             //Get distance and time
             const output = document.querySelector('#output');
-            output.innerHTML = "<div class='alert-info'>From: " + document.getElementById("from").value + ".<br />To: " + document.getElementById("to").value + ".<br /> Driving distance <i class='fas fa-road'></i> : " + result.routes[0].legs[0].distance.text + ".<br />Duration <i class='fas fa-hourglass-start'></i> : " + result.routes[0].legs[0].duration.text + ".</div>";
+            output.innerHTML = "<div class='alert-info'>" + " <i class='fas fa-road'></i> Driving distance: " + result.routes[0].legs[0].distance.text + ".</div>";
 
             //display route
             directionsDisplay.setDirections(result);
@@ -47,7 +47,7 @@ function calcRoute() {
             map.setCenter(myLatLng);
 
             //show error message
-            output.innerHTML = "<div class='alert-danger'><i class='fas fa-exclamation-triangle'></i> Could not retrieve driving distance.</div>";
+            output.innerHTML = "<div class='alert-danger'><i class='fas fa-exclamation-triangle'></i> No location have found, please set your location in the Profile.</div>";
         }
 
     });
@@ -64,7 +64,6 @@ var autocomplete1 = new google.maps.places.Autocomplete(input1, options);
 
 var input2 = document.getElementById("to");
 var autocomplete2 = new google.maps.places.Autocomplete(input2, options);
-
 
 
 infoWindow = new google.maps.InfoWindow();
@@ -120,3 +119,57 @@ function showPosition(position) {
   var lng = position.coords.longitude;
   map.setCenter(new google.maps.LatLng(lat, lng));
 }
+
+/*infoWindow = new google.maps.InfoWindow();
+
+  const locationButton = document.createElement("button");
+
+  locationButton.textContent = "Pan to Current Location";
+  locationButton.classList.add("custom-map-control-button");
+  map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+  locationButton.addEventListener("click", () => {
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          };
+
+          infoWindow.setPosition(pos);
+          infoWindow.setContent("Location found.");
+          infoWindow.open(map);
+          map.setCenter(pos);
+        },
+        () => {
+          handleLocationError(true, infoWindow, map.getCenter());
+        }
+      );
+    } else {
+      // Browser doesn't support Geolocation
+      handleLocationError(false, infoWindow, map.getCenter());
+    }
+  });
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(
+    browserHasGeolocation
+      ? "Error: The Geolocation service failed."
+      : "Error: Your browser doesn't support geolocation."
+  );
+  infoWindow.open(map);
+}
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    alert("Geolocation is not supported by this browser.");
+  }
+}
+function showPosition(position) {
+  var lat = position.coords.latitude;
+  var lng = position.coords.longitude;
+  map.setCenter(new google.maps.LatLng(lat, lng));
+}*/
